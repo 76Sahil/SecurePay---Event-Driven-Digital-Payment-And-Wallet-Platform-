ecaba0dc045108d76ccbcf9949177e84a8792c79
sahil <sahilmpaliwal@gmail.com>
2026-09-22 19:43:13 +0530

    Initial Project Structure

---
+++ backend/src/main/java/com/securepay/SecurepayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.securepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurepayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurepayBackendApplication.class, args);
+	}
+
+}
